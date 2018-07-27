c35970fbc9cfde2c3331aef0438eb512fce68349
USER <USER@USER-PC>
2018-07-27 18:03:34 +0530

    commit1.2

--- src/SeleniumWebDriver/SeleniumDay4ChromeBrowser.java
+++ src/SeleniumWebDriver/SeleniumDay4ChromeBrowser.java
@@ -1,11 +1,45 @@
 package SeleniumWebDriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class SeleniumDay4ChromeBrowser {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Webelement
 		
 		
+		// clear()
+		// click();
+		// sendkeys()
+		// getText()
+		// getAttribute()
+		// getTagName
+		// isDisplayed
+		// isSelected
+		// isEnable
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com/");
+		//
+		// to maximize the Browser
+		driver.manage().window().maximize();
+		WebElement ele = driver.findElement(By.xpath(".//*[@id='email']"));
+		// send the data from the username field
+		ele.sendKeys("Hello");
+		// is used to clear the data from the username field
+		ele.clear();
+		// this method are used to click the element in the field
+		ele.click();
+
+		String value = ele.getAttribute("class");
+		System.out.println(value);
+		
+		//to return the tag name from the pertucular element
+		String tag=ele.getTagName();
+		System.out.println(tag);
 
 	}
 
