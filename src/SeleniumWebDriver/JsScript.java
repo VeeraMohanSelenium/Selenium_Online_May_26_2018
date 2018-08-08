d1e54d8d0d863bea43d54756c05c73d97f4dffe4
USER <USER@USER-PC>
2018-08-08 18:31:46 +0530

    commit2.1

--- src/SeleniumWebDriver/JsScript.java
+++ src/SeleniumWebDriver/JsScript.java
@@ -17,19 +17,33 @@ public class JsScript {
 		driver.get("https://www.facebook.com/");
 
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("Hello");
+
+		// JavascriptExecutor is InterFace , in Not possible to create the
+		// object
+		// Using JavascriptExecutor click on hiden mode Elements
+		// Using ScrollBy
+		// arguments[0].scrollIntoView(): Scroll by Particular Element
+		
+		//arguments[0].click() : used to Click on Hiden Mode Elements
+		// JavascriptExecutor je=(JavascriptExecutor)driver;
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-
+		// js.executeScript("window.scrollBy(0,500)");
+		// (x,y)
 		js.executeScript("window.scrollBy(0,500)");
+//		Thread.sleep(3000);
+//		js.executeScript("window.scrollBy(0,-500)");
+		
+		
 
-		Thread.sleep(3000);
-
-		WebElement ele = driver.findElement(By.xpath("//*[@id='pass']"));
-
-		js.executeScript("arguments[0].scrollIntoView();", ele);
-
-		WebElement log = driver.findElement(By.xpath("//input[@value='Log In']"));
-
-		js.executeScript("arguments[0].click();", log);
+		 Thread.sleep(3000);
+		
+		 WebElement ele = driver.findElement(By.xpath("//*[@id='pass']"));
+		
+		 js.executeScript("arguments[0].scrollIntoView();", ele);
+		
+		 WebElement log = driver.findElement(By.xpath("//input[@value='Log In']"));
+		
+		 js.executeScript("arguments[0].click();", log);
 
 	}
 
--- src/SeleniumWebDriver/Popups.java
+++ src/SeleniumWebDriver/Popups.java
@@ -9,20 +9,39 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Popups {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException  {
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://www.rediff.com");
+
+		// Click on SignIn button
 		driver.findElement(By.xpath("//a[text()='Sign in']")).click();
-
+		// Click on Go button under SignIn page
 		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		// using Alert Interface to handle the Popups and Alerts
+		Alert ac = driver.switchTo().alert();
+		// useing getText() method get the value from the aleart box
+		String value = ac.getText();
+		System.out.println(value);
+		if (value.contains("Please enter a valid user name")) {
+			System.out.println("popup is verified with Valid");
+		} else {
+			System.out.println("popup is notverified ");
+		}
+		Thread.sleep(3000);
+		// Using accept() method to click on ok or Save in Popups
+		ac.accept();
 
-		Alert al = driver.switchTo().alert();
-		String alert_Text = al.getText();
-		al.sendKeys("Value");
+		// Using dismiss method to click on clancel button in popups
+		// ac.dismiss();
+
+		// Alert al = driver.switchTo().alert();
+		String alert_Text = ac.getText();
+		// Send the value from the aleart TextField
+		ac.sendKeys("Value");
 		System.out.println(alert_Text);
-		al.accept();
+		ac.accept();
 
 	}
 
